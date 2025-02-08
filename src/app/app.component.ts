f75237444becfc92653800c25d8532c2547b078e
sahandsavadi <sahanddev1@gmail.com>
2025-02-08 23:39:06 +0330

    fix
    Signed-off-by: sahandsavadi <sahanddev1@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MultiFormsComponent } from './components/multi-forms/multi-forms.component';
-import { DynamicFormComponent } from './components/dynamic-form/dynamic-form.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-root',
